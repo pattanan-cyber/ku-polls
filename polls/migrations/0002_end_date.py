9cd2cd15b70f133ea630cabbb65fd93afe1d69bd
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2021-10-27 16:36:16 +0700

    change background

---
+++ polls/migrations/0002_end_date.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='question',
+            name='end_date',
+            field=models.DateTimeField(null=True, verbose_name='end date'),
+        ),
+    ]
